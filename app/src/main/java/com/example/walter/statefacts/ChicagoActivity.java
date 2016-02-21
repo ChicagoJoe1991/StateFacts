c627819d69a5cd2fb6b2290dbc448d5c1b0be2d4
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-20 23:48:39 -0600

    create chicagoActivity; update ChicagoActivity TextView

--- app/src/main/java/com/example/walter/statefacts/ChicagoActivity.java
+++ app/src/main/java/com/example/walter/statefacts/ChicagoActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ChicagoActivity extends AppCompatActivity {
 
@@ -13,17 +14,10 @@ public class ChicagoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicago);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView textView = (TextView) findViewById(R.id.chicago);
+        textView.setText(R.string.chicago_title);
+
     }
 
 }
